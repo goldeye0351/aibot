91d7b65c4593faa6a06f42b568e3e56c282831ac
Goldeye0351 <goldeye0351@gmail.com>
2025-05-18 10:34:21 +0800

    href

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 //import Image from "next/image";
 import SplineScene from '@/components/hero/SplineScene'
 import { Hero } from "@/components/hero/demo";
+import { VideoText } from '@/components/hero/video-text';
 export default function Home() {
   return (
-    <div className="relative bg-background  ">
+    <a href="https://www.51xmi.com/blog/aibotnew" className="relative bg-background  ">
       <section className='relative h-screen flex flex-col lg:flex-row  justify-center lg:px-8'>
-      <SplineScene />
-          <Hero/>
-
+      <VideoText  src="https://r2.51xmi.com/pichub/ocean-small.webm/1745910238642/ocean-small.webm" >
+        51xMI.com
+        </VideoText>
+        <SplineScene />
+        <Hero/>
       </section>
-    </div>
+    </a>
   );
 }
